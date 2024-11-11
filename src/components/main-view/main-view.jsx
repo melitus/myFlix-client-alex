1d1ee06a84c7bb84de420a8debc7be8798900213
lorondo <mralexanderlindsay@gmail.com>
2024-11-10 17:45:55 -0800

    replaced movie array with empty array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,29 +5,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1, 
-      title: "Jurassic Park",
-      image: "https://m.media-amazon.com/images/I/61GHS6ht4uL._SL1500_.jpg",
-      genre: "Action",
-      director: "Steven Speilberg"
-    },
-    {
-      id: 2, 
-      title: "Raiders of the Lost Ark",
-      image: "https://m.media-amazon.com/images/I/91sD6F9q-DL._SL1500_.jpg",
-      genre: "Action",
-      director: "Steven Speilberg"
-    },
-    {
-      id: 3, 
-      title: "Ocean's Eleven",
-      image: "https://m.media-amazon.com/images/I/51NQ40oxXWL._SX342_.jpg",
-      genre: "Crime",
-      director: "Steven Soderbergh"
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectMovie, setSelectedMovie] = useState(null);
 
