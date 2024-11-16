45bf3e7b1bc389abb4b94b7f50514fd07a66b1a2
lorondo <mralexanderlindsay@gmail.com>
2024-11-16 15:36:28 -0800

    Updated main-view.jsx
    integrated login and sign-up views
    added in authentication checks

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,22 +1,47 @@
 import { MovieCard } from "../movie-card/movie-card";
-
 import { MovieView } from "../movie-view/movie-view";
-
 import { useState, useEffect } from "react";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const urlAPI = "https://movies-flix123-4387886b5662.herokuapp.com";
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser ? storedUser : null);
+  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
   const [selectMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch(urlAPI + "/movies")
+    if (!token) return;
+
+    fetch(urlAPI + "/movies", {
+      headers: { Authorization: `Bearer ${token}`},
+    })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return (
+      <>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+        or
+        <SignupView />
+      </>
+    );
+  }
+
+  }
 
   if (selectedMovie) {
     return (
@@ -32,12 +57,20 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard
+          key={movie.id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
         />
       ))}
+      <button 
+        onClick={() => {
+          setUser(null);
+          setUser(null);
+          localStorage.clear();
+        }}
+      >Logout</button>
     </div>
   );
 };
