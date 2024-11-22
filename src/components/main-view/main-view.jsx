17f26394fecfdda1f153861fcd0e181a1d0c2b15
lorondo <mralexanderlindsay@gmail.com>
2024-11-21 19:24:19 -0800

    added Row main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieView } from "../movie-view/movie-view";
 import { useState, useEffect } from "react";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import Row from "react-boostrap/Row";
 
 export const MainView = () => {
   const urlAPI = "https://movies-flix123-4387886b5662.herokuapp.com";
@@ -28,49 +29,45 @@ export const MainView = () => {
 
   if (!user) {
     return (
+      <Row>
+        {!user ? (
+          <>
+            <LoginView
+              onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+              }}
+            />
+            or
+            <SignupView />
+          </>
+    ) : selectedMovie ? (
+      <MovieView 
+      movie={selectedMovie} 
+      onBackClick={() => setSelectedMovie(null)} 
+      />
+    ) : movies.length === 0 ? (
+      <div>The list is empty!</div>
+    ) : (
       <>
-        <LoginView
-          onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }}
-        />
-        or
-        <SignupView />
-      </>
-    );
-  }
-
-  }
-
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  return (
-    <div>
-      {movies.map((movie) => (
+        {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           movie={movie}
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
           }}
-        />
-      ))}
-      <button 
-        onClick={() => {
-          setUser(null);
-          setUser(null);
-          localStorage.clear();
-        }}
-      >Logout</button>
-    </div>
-  );
-};
+      />
+    ))}
+    <button 
+      onClick={() => {
+        setUser(null);
+        setUser(null);
+        localStorage.clear();
+      }}
+    >Logout</button>
+      </>
+    )}
+  </Row>
+);
+  };
