41408d9cc380a8ff3d9a9c5bacd37ef1f267ab48
lorondo <mralexanderlindsay@gmail.com>
2024-11-23 20:47:19 -0800

    added profile-view beginning

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react";
+import {Form, Button, Row, Col, Container, Card} from "react-bootstrap";
+import { MovieCard } from "../movie-card/movie-card";
+
+export const ProfileView = ({user, token, movies, onUpdateUser, onDeregister}) => {
+  
+}
