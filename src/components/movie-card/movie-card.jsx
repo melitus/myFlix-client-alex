80effe2c45af01b91747ba648b51ccf824136a09
lorondo <mralexanderlindsay@gmail.com>
2024-12-29 10:34:49 -0800

    updated movie-card
    updated add favorites to be dynamic and add and remove favorite movies depending on whether a movie is favorite or not

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -23,26 +23,31 @@ export const MovieCard = ({ movie, user, updateFavorites}) => {
   // Handle toggle of the favorite movies
   const handleFavoriteToggle = () => {
     const movieID = movie._id;
+    const method = isFavorite ? "DELETE" : "POST";
     
     fetch(urlAPI + `/users/${loggedInUsername}/movies/${movieID}`, {
-        method: "POST",
-        body: JSON.stringify(),
+        method: method,
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("token")}`, // Token for authentication 
         },
       })
-    }
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .then((updatedUser) => {
+          console.log("Updated user:", updatedUser);
 
-    // console.log("User:", user);
-    // console.log("User's FavoriteMovies:", user?.FavoriteMovies);
-    
-    // //Toggle favorite status
-    // const updatedFavorites = isFavorite
-    //   ? user.FavoriteMovies.filter((id) => id !== movie._id) // Remove from favorites
-    //   : [...user.FavoriteMovies, movie._id]; // Add to favorites
-
-    // // Call the function passed via props to update the backend and user state
-    // updateFavorites(updatedFavorites);
+          // Update the frontend state wih the updated favorites
+          updateFavorites(updatedUser.FavoriteMovies);''
+        })
+        .catch((err) => {
+          console.error("Failed to update favorites:", err);
+        });
+    };
   };
 
   return (
@@ -73,7 +78,6 @@ export const MovieCard = ({ movie, user, updateFavorites}) => {
       </Card.Body>
     </Card>
   );
-};
 
 // Defined props constraints for the MovieCard
 MovieCard.propTypes = {
